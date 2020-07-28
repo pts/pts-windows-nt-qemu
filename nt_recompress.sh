43e63fbc8601a74c6e67f67e58e520201d6cb7e8
Peter Szabo <pts@fazekas.hu>
2020-07-29 01:26:20 +0200

    added rm of .bsb to nt_recompress.sh

--- nt_recompress.sh
+++ nt_recompress.sh
@@ -10,7 +10,7 @@ cd "${0%/*}"
 for NTD in "$@"; do
   mtools -c mcopy --version
   test -f "$NTD".img.bak || cp "$NTD".img "$NTD".img.bak
-  test -f "$NTD".bs || dd if="$NTD".img skip=32318 bs=1 count=450 of="$NTD".bs
+  test -f "$NTD".bsb || dd if="$NTD".img skip=32318 bs=1 count=450 of="$NTD".bsb
   mkdir "$NTD".rec
   # 7z gets confused by WINNT being a file.
   # (cd "$NTD".rec && 7z x ../"$NTD".img.bak) || exit "$?"
@@ -20,7 +20,7 @@ for NTD in "$@"; do
   gzip -cd <fat16hd.img.gz >"$NTD".fat16hd.img
   dd if="$NTD".fat16hd.img of="$NTD".img bs=8K conv=notrunc  # 140 MiB, 1 partition.
   rm -f "$NTD".fat16hd.img
-  dd if="$NTD".bs seek=32318 bs=1 count=450 of="$NTD".img conv=notrunc
+  dd if="$NTD".bsb seek=32318 bs=1 count=450 of="$NTD".img conv=notrunc
   # We lose permission bits, ownership, hidden--system attribute bits etc.
   mtools -c mcopy -i "$NTD".img@@32256 "$NTD".rec/* -s ::
   rm -rf "$NTD".rec
